b03564dc06fe6e03f46d55eb4b10c2930b650770
Polina <ivanetspolina@knu.ua>
2024-05-31 09:27:54 +0300

    test_categoty

--- gallery/tests.py
+++ gallery/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from gallery.models import Category, Image
+from django.core.files.uploadedfile import SimpleUploadedFile
+from datetime import date
 
-# Create your tests here.
+
+class CategoryTest(TestCase):
+    def test_category_creation1(self):
+        category = Category.objects.create(name="Nature")
+        self.assertEqual(category.name, "Nature")
+
+    def test_category_creation2(self):
+        category = Category.objects.create(name="Nature")
+        self.assertEqual(str(category), "Nature")
